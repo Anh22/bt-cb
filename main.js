2d4cd70a91f9cce62c9649ed7e81e75b3b842b50
unknown <phamquocanh072001@gmail.com>
2023-10-06 14:23:21 +0700

    bt

---
+++ main.js
@@ -0,0 +1,32 @@
+
+//Bài 1
+// let a = +prompt('Nhập điểm môn vật lý')
+// let b = +prompt('Nhập điểm môn Hóa học')
+// let c=+prompt('Nhập điểm môn Sinh học')
+// x=a+b+c;
+// v=x/3;
+//
+//
+// alert('Tổng điểm là: '+x);
+// alert('Điểm trung bình cả ba môn la: '+v)
+
+
+//Bài 2
+
+// let C=prompt("Nhập giá trị C")
+// F=(C/5)*9+32;
+// alert('Độ F ='+F);
+
+
+//Bài 3
+// let pi=3.14;
+// let R=prompt('Nhập bán kính hình tròn:');
+// S=R*R*pi;
+// alert('Diện tích hình tròn'+S);
+
+
+// Bài 4
+let  C=prompt('Nhập bán kính r = ');
+c=2*3.14*C;
+alert('Chu vi hình tròn là '+c);
+
